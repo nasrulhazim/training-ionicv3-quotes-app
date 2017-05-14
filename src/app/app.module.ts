832f7338472c0633fb807dfc04a44122c4734d8c
Nasrul Hazim <nasrulhazim.m@gmail.com>
2017-05-14 12:56:16 +0800

    read from json & create list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { HttpModule } from '@angular/http';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { QuotesListPage } from '../pages/quotes-list/quotes-list';
 import { QuotesDetailPage } from '../pages/quotes-detail/quotes-detail';
+
 
 @NgModule({
   declarations: [
@@ -18,7 +20,8 @@ import { QuotesDetailPage } from '../pages/quotes-detail/quotes-detail';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    HttpModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/quotes-list/quotes-list.ts
+++ src/pages/quotes-list/quotes-list.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+import { QuotesDetailPage } from '../quotes-detail/quotes-detail';
 
 /**
  * Generated class for the QuotesListPage page.
@@ -13,8 +16,35 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'quotes-list.html',
 })
 export class QuotesListPage {
+  quotesList = [];
+  filteredQuotes = [];
+  isfiltered: boolean;
+  constructor(public navCtrl: NavController, public navParams: NavParams, private http:Http) {
+    this.isfiltered = false;
+    this.http.get('quotes.json')
+      .map(res => res.json())
+      .subscribe(
+        data => {
+          this.quotesList = data.quotes;
+          console.log(this.quotesList);
+        },
+        err => console.error("Error: " + err),
+        () => console.log("Read Quotes Completed")
+      );
+  }
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  searchQuotes(event) {
+    if(event.target.value.length > 2) {
+      var filteredJson = this.quotesList.filter(function (row) {
+        if(row.author.indexOf(event.target.value) != -1) {
+          return true
+        } else {
+          return false;
+        }
+      });
+      this.isfiltered = true;
+      this.filteredQuotes = filteredJson;
+    }
   }
 
   ionViewDidLoad() {
